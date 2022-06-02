3f63fea97c7aea01dc6016d8d0d53ab3ca6580c9
meetkome <kvictory1010@gmail.com>
2022-06-02 16:24:26 -0700

    files long short

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	clrscr();
+	printf("Size of a char: %2d byte(s)\n" ,sizeof( char));
+
+	printf("Size of an int:  %2d byte(s) \n ", sizeof(int)); 
+
+	Printf("Size of a long int: %2d byte(s) \n ", sizeof(long int));
+
+	Printf("Size of a long  long int: %2d byte(s) \n ", sizeof(long long int));
+
+	Printf("Size of a float: %2d byte(s) \n ", sizeof(float));
+
+
+
+	return (0);
+}
