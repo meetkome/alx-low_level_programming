6efde6cdac456a6c8a88c724fa5bbeff09211fe1
meetkome <kvictory1010@gmail.com>
2022-06-02 16:33:58 -0700

    size of file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,18 +8,11 @@
 
 int main(void)
 {
-	clrscr();
-	printf("Size of a char: %2d byte(s)\n" ,sizeof( char));
 
-	printf("Size of an int:  %2d byte(s) \n ", sizeof(int)); 
-
-	Printf("Size of a long int: %2d byte(s) \n ", sizeof(long int));
-
-	Printf("Size of a long  long int: %2d byte(s) \n ", sizeof(long long int));
-
-	Printf("Size of a float: %2d byte(s) \n ", sizeof(float));
-
-
-
-	return (0);
+printf("Size of a char: %2d byte(s)\n" ,sizeof( char));
+printf("Size of an int:  %2d byte(s) \n ", sizeof(int)); 
+Printf("Size of a long int: %2d byte(s) \n ", sizeof(long int));
+Printf("Size of a long  long int: %2d byte(s) \n ", sizeof(long long int));
+Printf("Size of a float: %2d byte(s) \n ", sizeof(float));
+return (0);
 }
