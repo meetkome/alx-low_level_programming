36d0e59acf3289948e34d788841e38dda09c4fc6
meetkome <kvictory1010@gmail.com>
2022-06-05 10:47:02 -0700

    new commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,14 +9,14 @@ int main(void)
 	int ch;
 while (ch <= 99)
 {
-	putchar(ch / 10 + '0');
-	putchar(ch % 10 + '0');
+	putchar(ch / 10 + '1');
+	putchar(ch % 10 + '1');
 if (ch != 99)
 {
 	putchar(',');
 	putchar(' ');
 }
-ch--;
+ch++;
 }
 putchar('\n');
 return (0);
