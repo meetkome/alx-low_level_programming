2756d9cd71cb7e78ab9ca3540e74c0689efa06b7
meetkome <kvictory1010@gmail.com>
2022-06-05 06:14:55 -0700

    reverse alphabet

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,18 +5,15 @@
  *
  * Return: Always 0 (Success)
  *
- */                                                                                                                        
+ */
 
-                                                                                                                                               
-
-int main(void)                                                                                                                                 
-
-{                                                                                                                                              
-char ch;                                                                                                                                       
+int main(void)
+{
+char ch;
 char space = '\n';
 for (ch = 'z' ; ch >= 'a' ; ch--)
-{	
-       	putchar(ch);
+{
+	putchar(ch);
 }
 putchar(space);
 return (0);
