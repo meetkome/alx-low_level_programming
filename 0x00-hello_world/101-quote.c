e3cb95da4136c5742f462382a031e4ce177ef4e6
meetkome <kvictory1010@gmail.com>
2022-06-05 12:10:05 -0700

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+	printf("and that piece of art is useful\" - Dora Korpa, 2015-10-19\n");
 	return (1)
 }
