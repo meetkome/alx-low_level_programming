5ddf09cf696ad42ffdf1c38c934766242e4a4bba
meetkome <kvictory1010@gmail.com>
2022-06-05 12:04:30 -0700

    new commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,24 +11,17 @@ int main(void)
 {
 	int x, y, z;
 
-for (x = 0 ; x < 8 ; x++)
+while (n <= 99)
 {
-for (y = 0 ; y < 9 ; y++)
-{
-for (z = 0; z < 10 ; z++)
-{
-	putchar(x % 10 + '0');
-	putchar(y % 10 + '0');
-	putchar(z % 10 + '0');
-
-if (x == 7 && y == 8 && z == 9)
-	continue;
+	putchar(n % 10 + '0');
+	putchar(n % 10 + '0');
+	if (n != 99)
+	{
 	putchar(',');
-	putchar(' ');
-}
-}
-}
 	putchar('\n');
+}
+n++;
+}
 
 	return (0);
 }
