149c558125fcc33b5d6df317e7dabbbf74e4e0bb
meetkome <kvictory1010@gmail.com>
2022-06-05 06:26:01 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch;
+char space = '\n';
+for (ch = '0' ; ch <= '9'; c++)
+{
+	putchar(ch);
+}
+for (ch = 'a' ; c <= 'f' ; c++)
+{
+	putchar(ch);
+}
+putchar(space);
+return (0);
+}
