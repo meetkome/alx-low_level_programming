e8e0440e4cb523a76ecf5d621d7331fd1dd7e2c5
meetkome <kvictory1010@gmail.com>
2022-06-07 16:33:31 -0700

    24hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,8 +18,8 @@ void jack_bauer(void);
 		_putchar('0' + (hrs / 10));
 		_putchar('0' + (hrs % 10));
 		_putchar(':');
-		_putchar('0' + (min / 10))
-		_putchar('0' + (min % 10))
+		_putchar('0' + (min / 10));
+		_putchar('0' + (min % 10));
 		_putchar('\n');
 		min++;
 	}
