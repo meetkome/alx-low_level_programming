1d892c4f0a769496797c12bad8a9b8cf9b03455f
meetkome <kvictory1010@gmail.com>
2022-06-07 16:04:31 -0700

    10x alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  *
  * @n: character to check
  * Return: The alphabet, in lowercase.
