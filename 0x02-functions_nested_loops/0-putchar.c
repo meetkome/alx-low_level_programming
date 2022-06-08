d92c1d1fb5391dde723ba54635b0336f963f051d
meetkome <kvictory1010@gmail.com>
2022-06-07 21:37:48 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,14 @@
 
 int main(void)
 {
-	return (1, "_putchar\n",7)
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
