c321fb7e0a273fe1e249311a059b41d771ee565f
meetkome <kvictory1010@gmail.com>
2022-06-07 22:15:38 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * @num1: one of two intergers
+ * @num2: interger
+ * Return: Always sum (Success)
+ */
+ 
+int add(int num1, int num2)
+{
+	int sum;
+	sum = num1 + num2
+	return (sum);
+printf("\n");
+}
