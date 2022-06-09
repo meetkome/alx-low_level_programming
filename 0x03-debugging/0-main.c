c3bd95bc88528453365e35e98338a34407392aaa
meetkome <kvictory1010@gmail.com>
2022-06-09 01:35:46 -0700

    add main h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.c"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: prints correct ouput when input is 0
+ *
+ */
+
+int main(void)
+{
+	int n;
+	n = 0;
+	positive_or_negative(i);
+
+return (0);
+}
