78025d5c490fb557d927734eb47ea6bc08542801
meetkome <kvictory1010@gmail.com>
2022-06-10 10:52:33 -0700

    new commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,14 +9,12 @@
 
 void print_most_numbers(void)
 {
-	int ch;
-	for (ch = 0; ch <= 9; ch++)
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
 	{
-	if (ch == 2 || ch == 4)
-	{
-	continue;
-	}
-	_putchar(ch + '0');
+		if (!(ch == '2' || ch == '4'))
+			_putchar(ch);
 	}
 	_putchar('\n');
 }
