c642f8cf5459202b92fa97a217889f885256d51e
meetkome <kvictory1010@gmail.com>
2022-06-09 17:26:05 -0700

    Upeercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,12 +9,12 @@
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	char c;
+	c = 'C';
+	printf("Return value when uppercase character %c is passed to isupper(): %d", c, isupper(c));
+
+	c = '+';
+	printf("\nReturn value when another character %c is passed to is isupper(): %d", c, isupper(c))
+
+	return (0);
 }
