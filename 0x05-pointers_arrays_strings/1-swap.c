edc583df6c42e0d7df3bcf6d5abdbfaeb3e3dcda
meetkome <kvictory1010@gmail.com>
2022-06-13 07:50:32 -0700

    swap one value for another

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swat_int - a function that swaps the values of two integers.
+ * @a: evaluated value
+ * @b: evaluated value
+ *
+ * Return: 
+ */
+
+void swap_int(int *a, int *b)
+{
+	int n = *a;
+	*a = *b;
+	*b = n;
+}
