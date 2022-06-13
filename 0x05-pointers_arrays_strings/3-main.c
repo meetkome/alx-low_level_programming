074e89e47b5532287d4133f6344c5702ff58346b
meetkome <kvictory1010@gmail.com>
2022-06-13 08:57:57 -0700

    print strings in stdout

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - checks code
+ *
+ * Return: Always o
+ */
+
+int main(void)
+
+{
+char *str;
+
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+_puts(str);
+return (0);
+
+}
