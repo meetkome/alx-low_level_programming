e431fea7862f66037b573d21d765cfd91b79bc5e
meetkome <kvictory1010@gmail.com>
2022-06-13 10:35:21 -0700

    half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,6 +10,7 @@ void puts_half(char *str)
 {
 	int count = 0, i;
 	while (count >= 0)
+
 	{
 		if (str[count] == '\0')
 			break;
@@ -18,7 +19,7 @@ void puts_half(char *str)
 
 	if (count % 2 == 1)
 		i = count / 2;
-	else 
+	else
 		i = (count - 1) / 2;
 	
 	for (i++; i < count; i++)
