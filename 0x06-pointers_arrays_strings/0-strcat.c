6d51730db219d99c06aeb00d597d18d7a9fd33b2
meetkome <kvictory1010@gmail.com>
2022-06-15 03:08:54 -0700

    main 0 for more strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * *_strcat - This function appends the src string to the dest string, 
+ * _strcat - This function appends the src string to the dest string, 
  * overwriting the terminating null byte (\0) at the end of dest, 
  * and then adds a terminating null byte
  * @hello: first string to be cat
@@ -13,8 +13,8 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char hello;
-	char world;
+	char *dest = "hello";
+	char *src = "world";
 	printf("Concatenated String: %s\n",  strcat(hello, world));
 	return (0);
 }
