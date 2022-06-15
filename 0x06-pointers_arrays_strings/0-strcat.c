8e3985c80aa5a5512e7b9b1b23532cca3710c6a7
meetkome <kvictory1010@gmail.com>
2022-06-15 02:56:19 -0700

    main 0 for more strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_strcat - This function appends the src string to the dest string, 
+ * overwriting the terminating null byte (\0) at the end of dest, 
+ * and then adds a terminating null byte
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char dest;
+	char src;
+	printf("Concatenated String: %s\n",  strcat(dest,src));
+}
+
