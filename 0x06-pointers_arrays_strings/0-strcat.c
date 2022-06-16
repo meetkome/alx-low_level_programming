edafacf650a911e81876a502b017898474129b69
meetkome <kvictory1010@gmail.com>
2022-06-16 08:22:25 -0700

    switch str

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,28 +5,27 @@
  * @dest: destination
  * @src: source
  *
- * Return: dest
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
-	int i; 
-	int j;
+int i; 
+int j;
 
-	i = 0;
-	j = 0;
+i = 0;
+j = 0;
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
 }
 
