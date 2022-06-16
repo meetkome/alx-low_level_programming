6a64c1865547ebbe488fe63e8c90e4c25f27927e
meetkome <kvictory1010@gmail.com>
2022-06-16 15:10:20 -0700

    main for line of code

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/**
- * magic - check code
- * @n: variable
- * Return: 0
- */
-int magic(int n)
+ /* write your line of code here...
+  * Remember:
+  * - you are not allowed to use a
+  * - you are not allowed to modify p
+  * - only one statement
+  * - you are not allowed to code anything else than this line of code
+  */
+int main(void)
 {
 	int n;
 	int a[5];
@@ -12,14 +14,6 @@ int magic(int n)
 
 	a[2] = 1024;
 	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * -you are not allowed to use a
-	 * -you are not allowed to modify the variable p
-	 * - only one statement
-	 * do no code anything else than this line of code
-	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
