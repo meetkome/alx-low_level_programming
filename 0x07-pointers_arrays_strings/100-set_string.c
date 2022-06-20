8b43795e97259b3c88d8b87417c6ac982c1da62d
meetkome <kvictory1010@gmail.com>
2022-06-20 10:34:33 -0700

    pointer val to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - sets pointer to char
+ * @s: pointer set to pointer
+ * @to: pointer to char
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to);
+{
+	*s = to;
+}
