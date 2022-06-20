2a1fabeadea5393f3c49e3c077b1e1d4fbfd87d6
meetkome <kvictory1010@gmail.com>
2022-06-20 09:26:09 -0700

    locate substr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,7 +3,7 @@
 
 /**
  * _strstr - function that locates a substring.
- * @haystacks: The string to be scanned
+ * @haystack: The string to be scanned
  * @needle: string to be searched in haystacks
  *
  * Return: A value or null
