c3de618f1e0d23294eaa9dd78009937de908f77f
meetkome <kvictory1010@gmail.com>
2022-06-20 10:08:00 -0700

    print diagonal of a squar

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - function that prints the sum of the two diagonals
+ * @a: integer
+ * @size: int size
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	{
+		s1 = s1 + a[i];
+		a = a + size;
+	}
+	a = a - size;
+	for (i = 0; i < size; i++)
+	{
+		s2 = s2 + a[i];
+		a = a - size;
+	}
+	printf("%d, %d\n", s1, s2);
+}
