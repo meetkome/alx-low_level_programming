9ead7618a2c44b23bdb1f3ba496054c9669332ad
meetkome <kvictory1010@gmail.com>
2022-06-20 08:53:59 -0700

    length of str

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,13 +2,13 @@
 #include <string.h>
 
 /**
- * 
  * _strspn - gets the length of a prefix substring.
  * @s: Strings
  * @accept: to be compared to s
  *
  * Return: number of bytes
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = strspn(s, accept);
