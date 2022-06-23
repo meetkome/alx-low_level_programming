b66e0cc45abd9549ccb5bf72b8cf1bff278e27ce
meetkome <kvictory1010@gmail.com>
2022-06-23 05:32:43 -0700

    prime num

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,26 +9,26 @@
 
 int is_prime_number(int n)
 {
-	int i = 2;
-	int half = n / 2;
-	int temp = 0;
-	if (n <= 1)
+int i = 2;
+int half = n / 2;
+int temp = 0;
+if (n <= 1)
 
-	{
-		return (0);
-	}
-	if (i < half)
-	{
-		if (n % i == 0)
-	{
-	temp += 1;
-	}
-	i++;
-	}
-	if (temp == 0)
-	{
-	return (1);
-	}
-
+{
 	return (0);
+}
+if (i < half)
+{
+if (n % i == 0)
+{
+temp += 1;
+}
+i++;
+}
+if (temp == 0)
+{
+return (1);
+}
+
+return (0);
 }
