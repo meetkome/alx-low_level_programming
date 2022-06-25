ccc8d8e803c4c7f2748a310232f812a99e794d75
meetkome <kvictory1010@gmail.com>
2022-06-25 01:18:12 -0700

    new

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,12 +8,9 @@
 int _strlen(char *s)
 {
 	int len = 0;
-	int i = 0;
 
-	while (s[i] != '\0')
-	{
+	while (*s++)
 		len++;
-		i++;
-	}
+
 	return (len);
 }
