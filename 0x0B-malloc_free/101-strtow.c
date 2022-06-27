a7723c6775f71fdf75c084996acfae1cd0bfa362
meetkome <kvictory1010@gmail.com>
2022-06-27 11:36:31 -0700

    for main 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -63,7 +63,7 @@ char **strtow(char *str)
 				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
-						*tmp++ = str[start++];
+					*tmp++ = str[start++];
 				*tmp = '\0';
 				matrix[k] = tmp - c;
 				k++;
