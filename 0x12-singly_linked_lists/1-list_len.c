9f59b88add39e974ad53dfe519670b3a67c1ae94
meetkome <kvictory1010@gmail.com>
2022-07-15 04:34:02 -0700

    new commit to check

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,45 +1,24 @@
 #include "lists.h"
-#include <string.h>
 
 /**
- * add_node - a function that adds a node to the head of linked list
- * @head: pointer to a list_t pointer that points to the head struct
- * @str: pointer to string that adds as node
- *
- * Return: a pointer to new head of list, NULL on failure.
+ * list_len - Returns the number of elements in a linked `list_t` list
+ * @h: head of linked list
+ * Return: number of elements in list
  */
 
-list_t *add_node(list_t **head, const char *str)
+size_t list_len(const list_t *h)
 {
-	list_t *pres;
-	int len_;
+	const list_t *pres;
+	size_t n;
 
-	char *string_dup;
+	n = 0;
+	pres = h;
 
-	pres =  malloc(sizeof(list_t));
-	if (pres == NULL)
+	while (pres != NULL)
 	{
-		return (NULL);
+		pres = pres->next;
+		n++;
 	}
 
-	string_dup = strdup(str);
-	if (string_dup == NULL)
-	{
-		free(pres);
-		return (NULL);
-	}
-
-	len_ = 0;
-
-	while (str[len_])
-	{
-		len_++;
-	}
-
-	pres->len = len_;
-	pres->str = string_dup;
-	pres->next = *head;
-	*head = pres;
-
-	return (pres);
+	return (n);
 }
