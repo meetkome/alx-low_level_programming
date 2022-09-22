0278e47303530640327183110ed1e98c778a1046
meetkome <kvictory1010@gmail.com>
2022-09-22 12:24:09 +0100

    Header files for hash tables

--- 0x18-dynamic_libraries/math.c
+++ 0x18-dynamic_libraries/math.c
@@ -1,3 +1,11 @@
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
 int add(int a, int b)
 {
       return (a + b);
